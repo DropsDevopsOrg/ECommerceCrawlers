cd9b72693a7668b82890748eb3500f71d6c5c1ab
Hatcat123 <1599121712@qq.com>
2020-08-06 15:36:49 +0800

    :bug: [fix]增加忽略豆瓣音乐证书

--- OthertCrawler/0x06douban_music/0x06douban_music.py
+++ OthertCrawler/0x06douban_music/0x06douban_music.py
@@ -1,32 +1,32 @@
-import requests
 import re
-import csv
+
+import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 url = "https://music.douban.com/chart"
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, verify=False)
 html = response.text
 soup = BeautifulSoup(html, 'lxml')
 source = str(soup.find_all(attrs={"class": "col5"}))
-#print(source)
+# print(source)
 
-pattern = re.compile('<li.*?"green-num-box">(.*?)</span>.*?<a href="javascript:;">(.*?)</a>.*?<p>(.*?)</p>', re.S | re.I)#.*?<p>(.*?)</p>
+pattern = re.compile('<li.*?"green-num-box">(.*?)</span>.*?<a href="javascript:;">(.*?)</a>.*?<p>(.*?)</p>',
+                     re.S | re.I)  # .*?<p>(.*?)</p>
 items = re.findall(pattern, source)
 
-#写入csv文件
+# 写入csv文件
 """
 with open('db music.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerow(['排名', '歌名                  ', '歌手和播放数量'])
     writer.writerows(items)
 """
-#写入txt文件
+# 写入txt文件
 for item in items:
     string = str(item)
     with open('db music.txt', 'a') as f:
-        f.write(string+'\n')
+        f.write(string + '\n')
     f.close()
-
-
